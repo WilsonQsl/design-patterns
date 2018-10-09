8678f480bf4d1bc57e7dcd21669201a800c9f58e
wilson11314 <1092530289@qq.com>
2018-10-09 17:18:25 +0800

    单例模式

--- singleton-design/src/com/sun/SingLetonLayz.java
+++ singleton-design/src/com/sun/SingLetonLayz.java
@@ -1,9 +1,7 @@
 package com.sun;
+
 /**
- * 懒汉
- * 1、声明一个静态的类的引用
- * 2、写一个静态方法判断引用是否为空，如果为空，则new 一个对象给他
- * 3、返回一个对象回去
+ * 懒汉 1、声明一个静态的类的引用 2、写一个静态方法判断引用是否为空，如果为空，则new 一个对象给他 3、返回一个对象回去
  * 4、线程不安全的，在多线程层的情况下不可使用
  */
 public class SingLetonLayz {
@@ -19,18 +17,20 @@ public class SingLetonLayz {
 		return singLetonLayz;
 	}
 }
+
 /**
- * 缺点：效率太低了，每个线程在想获得类的实例时候，执行getInstance()方法都要进行同步。
- * 优点：线程安全
+ * 缺点：效率太低了，每个线程在想获得类的实例时候，执行getInstance()方法都要进行同步。 优点：线程安全
+ * 
  * @author Administrator
  *
  */
-class SingLetonLayzA{
+class SingLetonLayzA {
 	private static SingLetonLayzA singLetonLayz;
 
 	private SingLetonLayzA() {
 	}
-	//防止多线程的时候要加上synchronized
+
+	// 防止多线程的时候要加上synchronized
 	public static synchronized SingLetonLayzA getIntance() {
 		if (singLetonLayz == null) {
 			singLetonLayz = new SingLetonLayzA();
@@ -38,21 +38,24 @@ class SingLetonLayzA{
 		return singLetonLayz;
 	}
 }
+
 /**
  * 双重检查[推荐用]
+ * 
  * @author Administrator
  *
  */
-class SingLetonLayzB{
+class SingLetonLayzB {
 	private static SingLetonLayzB singLetonLayz;
 
 	private SingLetonLayzB() {
 	}
-	//防止多线程的时候要加上synchronized
+
+	// 防止多线程的时候要加上synchronized
 	public static SingLetonLayzB getIntance() {
 		if (singLetonLayz == null) {
-			synchronized (SingLetonLayzB.class){
-				if(singLetonLayz == null){
+			synchronized (SingLetonLayzB.class) {
+				if (singLetonLayz == null) {
 					singLetonLayz = new SingLetonLayzB();
 				}
 			}
@@ -60,31 +63,36 @@ class SingLetonLayzB{
 		return singLetonLayz;
 	}
 }
+
 /**
  * 静态内部类[推荐用]
+ * 
  * @author Administrator
  *
  */
 class SingletonC {
 
-    private SingletonC() {}
+	private SingletonC() {
+	}
 
-    private static class SingletonInstance {
-        private static final SingletonC INSTANCE = new SingletonC();
-    }
+	private static class SingletonInstance {
+		private static final SingletonC INSTANCE = new SingletonC();
+	}
 
-    public static SingletonC getInstance() {
-        return SingletonInstance.INSTANCE;
-    }
+	public static SingletonC getInstance() {
+		return SingletonInstance.INSTANCE;
+	}
 }
+
 /**
  * 枚举[推荐用]
+ * 
  * @author Administrator
  *
  */
 enum SingletonD {
-    INSTANCE;
-    public void whateverMethod() {
+	INSTANCE;
+	public void whateverMethod() {
 
-    }
+	}
 }
