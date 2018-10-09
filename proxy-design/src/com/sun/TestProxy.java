ccd3ad464122e10eab34d6f8568b056f28b15669
wilson11314 <1092530289@qq.com>
2018-10-09 17:09:24 +0800

    代理模式

--- proxy-design/src/com/sun/TestProxy.java
+++ proxy-design/src/com/sun/TestProxy.java
@@ -1,18 +1,29 @@
 package com.sun;
 
+/**
+ * 代理模式：https://www.cnblogs.com/qifengshi/p/6566752.html
+ * 
+ * 所谓代理模式是指客户端并不直接调用实际的对象，而是通过调用代理，来间接的调用实际的对象。
+ * 
+ * 案例：在现实生活中有很多歌手在同一个地方要举办演唱会，在举办演唱会的同时他们需要做三件事，准备演唱会，某某歌手唱歌，收场
+ * 然而唱歌就是歌手需要做的，而且每个歌手的演唱方式都不同，因此可以定义一个唱歌的主题接口（Subject），具体的演唱细节
+ * 交给歌手处理（真实主题类，实现Subject接口），但是举办演唱会需要委托工作人员（准备演唱会，收场）这里就需要一个代理类（ProxySubject）
+ * 代理歌手做这两件事，
+ *
+ */
 public class TestProxy {
 	public static void main(String[] args) {
 		// HZ hz = new HZ();
 		// hz.prepare();
 		// hz.sing();
 		// hz.shutDowm();
-		new ProxySubject(new Jack()).make();
+		new ProxySubject(new AndyLau()).make();
 	}
 }
 
-class HZ implements Subject {
+class AndyLau implements Subject {
 	// public void prepare() {
-	// System.out.println("准备唱歌");
+	// System.out.println("准备演唱会");
 	// }
 
 	public void sing() {
@@ -24,9 +35,9 @@ class HZ implements Subject {
 	// }
 }
 
-class Jack implements Subject {
+class AlanTam implements Subject {
 	// public void prepare() {
-	// System.out.println("准备唱歌");
+	// System.out.println("准备演唱会");
 	// }
 
 	public void sing() {
@@ -50,7 +61,7 @@ class ProxySubject {
 	}
 
 	public void prepare() {
-		System.out.println("准备唱歌");
+		System.out.println("准备演唱会");
 	}
 
 	public void shutDowm() {
@@ -61,7 +72,6 @@ class ProxySubject {
 		this.prepare();
 		this.subject.sing();
 		this.shutDowm();
-		
-		
+
 	}
 }
